b0d98a7c24a2940c1702be05f8202c562f1760f6
gaydailan <ilang@iguaz.io>
2018-12-30 13:34:16 +0200

    Update utils.py

--- pinterest/utils.py
+++ pinterest/utils.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import urllib
+from six import string_types
 
 
 def url_encode(query):
-    if isinstance(query, basestring):
+    if isinstance(query, string_types):
         query = urllib.quote_plus(query)
     else:
         query = urllib.urlencode(query)
